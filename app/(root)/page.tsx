3c878f891fdc5564e976a7557586e7e007dbc89d
HassanShanJava <hassanshan675@gmail.com>
2023-08-24 01:25:16 +0500

    maded initally setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Hello Admin</p>
+      <Button size="lg" variant="outline">
+        Test
+      </Button>
+    </>
+  );
+}
