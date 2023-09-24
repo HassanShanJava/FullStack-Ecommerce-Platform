3577f853077170814a9d8ac883986917b4895a0f
HassanShanJava <hassanshan675@gmail.com>
2023-09-24 23:18:06 +0500

    working on setting form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,19 +1,29 @@
-import { Store } from '@prisma/client'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import prismadb from "@/lib/prismadb";
+import { useAuth } from "@clerk/nextjs";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
 
-interface SettingsFormProps{
-  initialData:Store
+interface SettingsFormProps {
+  initialData: Store;
 }
 
-const SettingsForm:React.FC<SettingsFormProps> = ({params}) => {
+const SettingsForm: React.FC<SettingsFormProps> = async ({ initialData }) => {
   return (
-    <div>
-      <div>
-        guh jh
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preference" />
+        <Button variant={"destructive"} size="icon" onClick={() => ({})}>
+          <Trash className="h-4 w-4" />
+        </Button>
       </div>
-      <p></p>
-    </div>
-  )
-}
+      <Separator/>
+    </>
+  );
+};
 
-export default SettingsForm
+export default SettingsForm;
